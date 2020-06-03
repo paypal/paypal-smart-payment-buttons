ea9f9d709c2e7c437b1d4d96a413ced8659f87ba
Daniel Brain <daniel@brainblocks.io>
2020-06-02 18:40:50 -0700

    Add permanent payerID validation in onApprove

--- src/props/onApprove.js
+++ src/props/onApprove.js
@@ -263,19 +263,15 @@ export function getOnApprove({ intent, onApprove = getDefaultOnApprove(intent), 
                 }).flush();
 
             if (!billingToken && !subscriptionID) {
-                getLogger().info(`on_approve_payer_id_${ payerID ? 'present' : 'not_present' }`, {
-                    orderID, paymentID, billingToken, subscriptionID
-                }).flush();
-            }
-
-            if (!payerID) {
-                getSupplementalOrderInfo.reset();
+                if (!payerID) {
+                    getLogger().error('onapprove_payerid_not_present', { orderID }).flush();
+                    throw new Error(`payerID not present in onApprove call`);
+                }
             }
 
             return getSupplementalOrderInfo(orderID).then(supplementalData => {
                 intent = intent || (supplementalData && supplementalData.checkoutSession && supplementalData.checkoutSession.cart && supplementalData.checkoutSession.cart.intent);
                 billingToken = billingToken || (supplementalData && supplementalData.checkoutSession && supplementalData.checkoutSession.cart && supplementalData.checkoutSession.cart.billingToken);
-                payerID = payerID || (supplementalData && supplementalData.checkoutSession && supplementalData.checkoutSession.buyer && supplementalData.checkoutSession.buyer.userId);
                 
                 const data = { orderID, payerID, paymentID, billingToken, subscriptionID, facilitatorAccessToken, authCode };
                 const actions = buildXApproveActions({ orderID, paymentID, payerID, intent, restart, subscriptionID, facilitatorAccessToken, buyerAccessToken, partnerAttributionID, forceRestAPI });
