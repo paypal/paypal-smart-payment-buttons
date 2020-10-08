6ee9d64beace293643cb762a238409921cd93016
Amy Egan <8247105+amyegan@users.noreply.github.com>
2020-10-08 10:05:06 -0400

    Add orderId to native app switch logging

--- src/payment-flows/native.js
+++ src/payment-flows/native.js
@@ -461,9 +461,9 @@ function initNative({ props, components, config, payment, serviceData } : InitOp
     });
 
     const detectAppSwitch = once(({ sessionUID, orderId } : {| sessionUID : string, orderId : string |}) => {
-        getLogger().info(`native_detect_app_switch`, { orderId }).track({
-            [FPTI_KEY.TRANSITION]: FPTI_TRANSITION.NATIVE_DETECT_APP_SWITCH,
-            'info_msg':            orderId // [FPTI_KEY.INFO_MSG]:   orderId
+        getLogger().info(`native_detect_app_switch`).track({
+            [FPTI_KEY.TRANSITION]:      FPTI_TRANSITION.NATIVE_DETECT_APP_SWITCH,
+            [FTPI_CUSTOM_KEY.INFO_MSG]: orderId
         }).flush();
 
         return connectNative({ sessionUID }).setProps();
