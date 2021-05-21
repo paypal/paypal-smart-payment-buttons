ad7bf371da1352e3dfb075a3a12bb0e475134beb
Steve Mask <smask@paypal.com>
2021-05-21 16:50:58 -0400

    refactor

--- server/components/qrcode/constants.js
+++ server/components/qrcode/constants.js
@@ -1,8 +1,7 @@
 /* @flow */
 
+export const VENMO_BLUE : string = '#3D93CE';
 export const EVENT : {| RENDER : string, ERROR : string |} = {
     RENDER: 'qrcode_render',
     ERROR:  'qrcode_error'
 };
-
-export const VENMO_BLUE : string = '#3D93CE';
