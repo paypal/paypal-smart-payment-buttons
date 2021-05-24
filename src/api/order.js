49b46b3348d572b8a9bdb07e9237d8823e57581d
Amy Egan <amyegan@paypal.com>
2021-05-24 16:54:28 -0400

    fix lint errors

--- src/api/order.js
+++ src/api/order.js
@@ -470,6 +470,7 @@ export function approveOrder({ orderID, planID, buyerAccessToken } : ApproveOrde
             [ HEADERS.CLIENT_CONTEXT ]: orderID
         }
     }).then(({ approvePayment }) => {
+        // eslint-disable-next-line no-console
         console.log('@@@ approve payment', approvePayment);
         setBuyerAccessToken(approvePayment.buyer.auth.accessToken);
         return {
@@ -758,6 +759,7 @@ export function payWithNonce({ orderID, paymentMethodNonce, clientID, branded = 
         }
     }).then(({ approvePaymentWithNonce }) => {
         getLogger().info('pay_with_paymentMethodNonce', JSON.stringify(approvePaymentWithNonce));
+        // eslint-disable-next-line no-console
         console.log('@@@ pay with nonce returned', approvePaymentWithNonce);
         setBuyerAccessToken(approvePaymentWithNonce.buyer.auth.accessToken);
         return {
